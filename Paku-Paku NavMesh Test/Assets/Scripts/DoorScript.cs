0fd3b40371ab24ce6c84e2075344a4510169b10b
Neil Angelo Almaznor <60123816+naAlmanzor@users.noreply.github.com>
2023-09-22 15:28:33 +0800

    Created map, added door mechanic

---
+++ Paku-Paku NavMesh Test/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public Transform doorPos;
+    bool isTriggered;
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Enemy"){
+            Debug.Log("ENEMY HIT TRIGGER!");
+            doorPos.position += new Vector3(4, 0, 0);
+            isTriggered = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        doorPos.position -= new Vector3(4, 0, 0);
+        isTriggered = false;
+    }
+}
